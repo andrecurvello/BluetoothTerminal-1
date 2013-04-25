6318742f6894cf1a6507ba46984be6286c39e7f6
Ruben R. Schmelzer <ruben.schmelzer@gmx.de>
2013-04-25 10:44:34 +0200

    Version 0.1 - Project start
    BT Terminal

---
+++ gen/de/dit/btt/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.dit.btt;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/de/dit/btt/BTTMainActivity.java
@@ -0,0 +1,23 @@
+package de.dit.btt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BTTMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bttmain);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bttmain, menu);
+        return true;
+    }
+    
+}
