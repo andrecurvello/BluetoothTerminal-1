b61db288f96538358699bcecd16eeae0c82eb5b6
Ruben R. Schmelzer <email@ruben-schmelzer.de>
2013-04-29 21:29:38 +0200

    Added global vars for GUI

--- src/de/dit/btt/BTTMainActivity.java
+++ src/de/dit/btt/BTTMainActivity.java
@@ -3,13 +3,22 @@ package de.dit.btt;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BTTMainActivity extends Activity {
+	
+	private TextView 	mTvOutput;
+	private EditText 	mEtInput;
+	private Button 		mBtSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bttmain);
+        
+        initGUI();
          
         
     }
@@ -21,5 +30,13 @@ public class BTTMainActivity extends Activity {
         getMenuInflater().inflate(R.menu.bttmain, menu);
         return true;
     }
+    
+    private void initGUI(){
+    	
+    	//mTvOutput = (TextView) findViewById(R.id.);
+    	 
+    	
+    }
+    
     
 }
