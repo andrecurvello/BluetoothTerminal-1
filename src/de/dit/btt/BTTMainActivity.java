87171d0d410a433979c920b453aa7ec2f33a9b52
Ruben R. Schmelzer <email@ruben-schmelzer.de>
2013-04-29 22:18:16 +0200

    RS: Implemented Thread to message from Bluetooth Device to Main Thread.

--- src/de/dit/btt/BTTMainActivity.java
+++ src/de/dit/btt/BTTMainActivity.java
@@ -1,6 +1,8 @@
 package de.dit.btt;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
@@ -15,19 +17,7 @@ public class BTTMainActivity extends Activity {
 	private EditText 	mEtInput;
 	private Button 		mBtSend;
 	
-	private OnClickListener handleSend = new OnClickListener(){
-
-		@Override
-		public void onClick(View arg0) {
-			 // TODO: Handle Send Button
-			
-		}
-    	
-    }; 
-	
-
-
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bttmain);
@@ -46,15 +36,75 @@ public class BTTMainActivity extends Activity {
     }
     
     private void initGUI(){
+		
+		mTvTerminal = (TextView) findViewById(R.id.tv_terminal);
+		mEtInput 	= (EditText) findViewById(R.id.et_input);
+		mBtSend 	= (Button)   findViewById(R.id.bt_send);
+		
+		mBtSend.setOnClickListener(handleSend);
+	
+		
+	}
+
+	private OnClickListener handleSend = new OnClickListener(){
+	
+		@Override
+		public void onClick(View arg0) {
+			 // TODO: Handle Send Button
+			
+		}
+		
+	};
+	
+	
+
+	public Handler handleBluetoothMessage = new Handler(){
+		
+		@Override
+		public void handleMessage(Message msg){
+			
+			// This is the place where the main activity thread receives messages
+			// Other threads will send messages to main here, pls handle
+			super.handleMessage(msg);
+			
+			mTvTerminal.append("Bluetooth Message: " + msg.toString() + "\n");
+	
+			
+		}
+		
+		
+		
+	};
+
+	public class BluetoothRunner implements Runnable {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+			while(!Thread.currentThread().isInterrupted()){
+				
+				Message msg = Message.obtain(); // Message pool
+				msg.what = 999; // custom ID
+		
+				Bundle b = new Bundle();
+				b.putString("Broadcast", "alive!");
+				msg.setData(b);
+			 	
+				BTTMainActivity.this.handleBluetoothMessage.sendMessage(msg);
+				
+				try {
+					
+					Thread.sleep(1000);					
+					
+				} catch (Exception e){
+					
+					
+				}
+			
+			
+		}
     	
-    	mTvTerminal = (TextView) findViewById(R.id.tv_terminal);
-    	mEtInput 	= (EditText) findViewById(R.id.et_input);
-    	mBtSend 	= (Button)   findViewById(R.id.bt_send);
-    	
-    	mBtSend.setOnClickListener(handleSend);
-    
     	
     }
-    
-    
-}
+}}
